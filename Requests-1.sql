2f1664041bfd4946cfe2831852414dbdf23a3134
SEREGA4332 <148464615+SEREGA4332@users.noreply.github.com>
2024-02-08 12:44:31 +0300

    Update Requests-1.sql

--- Requests-1.sql
+++ Requests-1.sql
@@ -1,46 +1,39 @@
-CREATE TABLE IF NOT EXISTS  Musical_genres(
-	id SERIAL PRIMARY KEY,
-	NameMusgen VARCHAR(80) NOT NULL
+CREATE TABLE IF NOT EXISTS genres(
+	id SERIAL primary key,
+	genres_name VARCHAR(40) not null
 );
-
-CREATE TABLE IF NOT EXISTS  Executors(
-	id SERIAL PRIMARY KEY,
-	NameExecut VARCHAR(80) NOT NULL
+CREATE TABLE IF NOT EXISTS artists(
+	id SERIAL primary key,
+	nickname VARCHAR(40) not null
 );
-
-CREATE TABLE IF NOT EXISTS Musgen_Execut (
-	musgen_id INTEGER REFERENCES Musical_genres(id),
-	execut_id INTEGER REFERENCES Executors(id),
-	CONSTRAINT ME PRIMARY KEY (musgen_id , execut_id)
+CREATE TABLE IF NOT EXISTS artistsgenres(
+	id SERIAL primary key,
+	artists_id INTEGER not null references artists(id),
+	genres_id INTEGER not null references genres(id)
 );
-
-CREATE TABLE IF NOT EXISTS  Albums(
-	id SERIAL PRIMARY KEY,
-	NameAlbum VARCHAR(80) NOT null,
-	YearAlbum INTEGER
+CREATE TABLE IF NOT EXISTS albums(
+	id SERIAL primary key,
+	albums_name VARCHAR(40) not null,
+	release_date DATE not null
 );
-
-CREATE TABLE IF NOT EXISTS Execut_Album (
-	album_id INTEGER REFERENCES Albums(id),
-	execut_id INTEGER REFERENCES Executors(id),
-	CONSTRAINT EA PRIMARY KEY (album_id , execut_id)
+CREATE TABLE IF NOT EXISTS artistsalbums(
+	id SERIAL primary key,
+	artists_id INTEGER not null references artists(id),
+	albums_id INTEGER not null references albums(id)
 );
-
-CREATE TABLE IF NOT EXISTS Tracks (
-	id SERIAL PRIMARY KEY,
-	album_id INTEGER REFERENCES Albums(id),
-	NameTrack VARCHAR(40) NOT null,
-	Durarion INTEGER
-	);
-
-CREATE TABLE IF NOT EXISTS Collections (
-	id SERIAL PRIMARY KEY,
-	NameCollect VARCHAR(40) NOT null,
-	YearCollect INTEGER
-	);
-
-CREATE TABLE IF NOT EXISTS Track_Collect (
-	track_id INTEGER REFERENCES Tracks(id),
-	collect_id INTEGER REFERENCES Collections(id),
-	CONSTRAINT TC PRIMARY KEY (track_id , collect_id)
+CREATE TABLE IF NOT EXISTS tracks(
+	id SERIAL primary key,
+	tracks_name VARCHAR(40) not null,
+	duration INTEGER not null,
+	albums_id INTEGER not null references albums(id)
+);
+CREATE TABLE IF NOT EXISTS collections(
+	id SERIAL primary key,
+	collections_name VARCHAR(40) not null,
+	release_date DATE not null
+);
+CREATE TABLE IF NOT EXISTS trackscollections(
+	id SERIAL primary key,
+	tracks_id INTEGER not null references tracks(id),
+	collections_id INTEGER not null references collections(id)
 );
