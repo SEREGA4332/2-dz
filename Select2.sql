7e4080ee2ce2b79198bf009a0562347cdd88b698
SEREGA4332 <148464615+SEREGA4332@users.noreply.github.com>
2024-02-21 17:56:11 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -1,11 +1,29 @@
-SELECT album_name, year_of_release FROM Album WHERE year_of_release = 2018;
+-- Задание 2
+-- название и продолжительность самого длительного трека
+SELECT nametrack, lengthtrack
+FROM track
+WHERE lengthtrack = (SELECT MAX(lengthtrack) FROM track);
 
-SELECT track_name, duration FROM track ORDER BY duration DESC LIMIT 1;
+SELECT nametrack, lengthtrack
+FROM track
+ORDER by lengthtrack DESC
+LIMIT 1;
 
-SELECT track_name, duration FROM track WHERE duration >= 210;
+-- название треков, продолжительность которых не менее 3,5 минут
+SELECT nametrack
+FROM track
+WHERE lengthtrack > 210;
 
-SELECT collection_name FROM collection WHERE year_of_release >=2018 AND year_of_release <=2020;
+-- названия сборников, вышедших в период с 2018 по 2020 год включительно
+SELECT nametrack, releasealbum FROM album a
+LEFT JOIN track t ON a.albumid = t.albumid
+WHERE releasealbum BETWEEN 2018 AND 2020;
 
-SELECT name FROM singer WHERE name NOT LIKE '% %';
+-- исполнители, чьё имя состоит из одного слова
+SELECT singername FROM singer m 
+WHERE singername NOT LIKE '% %';
 
-SELECT track_name FROM track WHERE track_name LIKE '%my%';
+-- название треков, которые содержат слово «мой» или «my»
+SELECT nametrack
+FROM track
+where LOWER(nametrack) like '%my%';
