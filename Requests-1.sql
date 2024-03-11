284a67405cd2facd869b4938cdc667c5355a51ed
SEREGA4332 <148464615+SEREGA4332@users.noreply.github.com>
2024-03-11 17:11:29 +0300

    Update Requests-1.sql

--- Requests-1.sql
+++ Requests-1.sql
@@ -1,50 +1,46 @@
-CREATE TABLE IF NOT EXISTS Genre(
-	genre_id SERIAL PRIMARY KEY,
-	genre_name VARCHAR(40) NOT NULL UNIQUE 
+CREATE TABLE IF NOT EXISTS Genre (
+    Genre_ID SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL
 );
 
-
-
-CREATE TABLE IF NOT EXISTS Singer(
-	singer_id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL
+CREATE TABLE IF NOT EXISTS Performer (
+    Performer_ID SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS Genres_Singers (
-singer_id INTEGER REFERENCES Singer(singer_id),
-genre_id INTEGER REFERENCES Genre(genre_id),
-CONSTRAINT pk PRIMARY KEY(singer_id, genre_id)
+CREATE TABLE IF NOT EXISTS Genre_Performer (
+    Genre_ID INTEGER REFERENCES Genre(Genre_ID),
+    Performer_ID INTEGER REFERENCES Performer(Performer_ID),
+    CONSTRAINT G_P PRIMARY KEY (Genre_ID, Performer_ID)
 );
 
-CREATE TABLE IF NOT EXISTS Album(
-	album_id SERIAL PRIMARY KEY,
-	album_name VARCHAR(80) NOT NULL,
-	year_of_release INTEGER NOT NULL CHECK(1990<=year_of_release AND year_of_release <=2023)
+CREATE TABLE IF NOT EXISTS Album (
+    Album_ID SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    release_date date
 );
 
-CREATE TABLE IF NOT EXISTS Albums_Singers (
-	singer_id INTEGER REFERENCES Singer(singer_id),
-	album_id INTEGER REFERENCES Album(album_id),
-	CONSTRAINT ak PRIMARY KEY(singer_id, album_id)
+CREATE TABLE IF NOT exists Performer_Album (
+    Performer_ID INTEGER REFERENCES Performer(Performer_ID),
+    Album_ID INTEGER REFERENCES Album(Album_ID),
+    CONSTRAINT P_A PRIMARY KEY (Album_ID, Performer_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Track (
-	track_id SERIAL PRIMARY KEY,
-	track_name VARCHAR(80) NOT NULL,
-	duration INTEGER NOT NULL CHECK(duration>=30 AND duration <=600),
-	album_id INTEGER NOT NULL REFERENCES Album(album_id)
+    Track_ID SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    Duration time,
+    Album INTEGER REFERENCES Album(Album_ID)
 );
-
 
 CREATE TABLE IF NOT EXISTS Collection (
-	collection_id SERIAL PRIMARY KEY,
-	collection_name VARCHAR(80) NOT NULL,
-	year_of_release INTEGER NOT NULL CHECK(2000<=year_of_release AND year_of_release <=2023)
+    Collection_ID SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    release_date date
 );
 
-CREATE TABLE IF NOT EXISTS track_collection (
-	track_id INTEGER REFERENCES Track(track_id),
-	collection_id INTEGER REFERENCES Collection(collection_id),
-	CONSTRAINT ck PRIMARY KEY(track_id, collection_id)
+CREATE TABLE IF NOT exists Track_Collection (
+    Track_ID INTEGER REFERENCES Track(Track_ID),
+    Collection_ID INTEGER REFERENCES Collection(Collection_ID),
+    CONSTRAINT T_C PRIMARY KEY (Track_ID, Collection_ID)
 );
