f1f52810ff3f0ce61cb5a14da989374e4d3ba096
SEREGA4332 <148464615+SEREGA4332@users.noreply.github.com>
2024-05-13 13:14:35 +0300

    Update Select.sql

--- Select.sql
+++ Select.sql
@@ -1,50 +1,42 @@
 --Название и продолжительность самого длительного трека.
-select name, duration from track
-where duration = (select max(duration) from track);
+SELECT TrackName, Length FROM Tracks
+WHERE Length = (SELECT MAX(Length) FROM Tracks);
 
 --Название треков, продолжительность которых не менее 3,5 минут.
-select name from track
-where duration >= '00:03:30.0000000';
+SELECT TrackName FROM Tracks
+WHERE Length >= 3.5;
 
 --Названия сборников, вышедших в период с 2018 по 2020 год включительно.
-select name from collection
-where release_date >= '2018-01-01' and release_date <= '2020-12-31';
+SELECT AlbumName FROM Albums
+WHERE ReleaseYear BETWEEN 2018 AND 2020;
 
 --Исполнители, чьё имя состоит из одного слова.
-select name from performer
-where name not like '% %';
+SELECT ArtistName FROM Artists
+WHERE ArtistName NOT LIKE '% %';
 
 --Название треков, которые содержат слово «мой» или «my».
-select name from track
-where name like '% мой %' or name like '% my %';
+SELECT TrackName FROM Tracks
+WHERE TrackName LIKE '%мой%' 
+OR TrackName LIKE '%my%';
 
---Количество исполнителей в каждом жанре.
-SELECT name, COUNT(genre_performer.genre_id) AS quantity FROM genre 
-LEFT JOIN genre_performer ON genre_performer.genre_id = genre.genre_id
+-- количество исполнителей в каждом жанре
+SELECT name, COUNT(genre_performer.genre_id) AS num_artists FROM artists
 GROUP BY name;
   
---Количество треков, вошедших в альбомы 2019–2020 годов (для получения значимого ответа изменим дату релизов на 1996 - 1997гг)
-select count(track.track_id) as quantity from track
-left join album on track.album = album.album_id
-where album.release_date >= '1996-01-01' and album.release_date <= '1997-12-31';
+--Количество треков, вошедших в альбомы 2019–2020 годов
+SELECT COUNT(track.track_id) AS num_tracks FROM tracks
+WHERE release_year BETWEEN 2019 AND 2020;
 
 --Средняя продолжительность треков по каждому альбому.
-select album.name, avg(track.duration) from album
-left join track on album.album_id = track.album
-group by album.name;
+SELECT album_id, AVG(duration) AS avg_duration FROM tracks
+GROUP BY album_id;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году.
-select performer.name from performer
-join performer_album pa on pa.performer_id = performer.performer_id 
-join album on pa.album_id = album.album_id 
-where extract(year from album.release_date) != 2020;
+SELECT artist_name FROM artists
+WHERE artist_id NOT IN (
+SELECT artist_id FROM albums
+WHERE release_year = 2020);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (Кровосток).
-select collection.name from collection
-join track_collection tc on tc.collection_id = collection.collection_id 
-join track on tc.track_id = track.track_id 
-join album a on a.album_id = track.album 
-join performer_album pa on pa.album_id = a.album_id 
-join performer p on p.performer_id = pa.performer_id 
-where p."name" = 'Кровосток'
-group by collection.name;
+SELECT collection_name FROM collections
+WHERE artist_id = (SELECT artist_id FROM artists WHERE artist_name = 'Кровосток');
